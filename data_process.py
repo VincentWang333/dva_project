b9357a5ff2d1aa0aacedf3bd540f1f031b83fba6
vincentwang333 <vincentwang333@outlook.com>
2022-10-26 00:25:33 -0400

    automation data clean up

--- data_process.py
+++ data_process.py
@@ -1,14 +1,19 @@
 import os
 import pandas
 from pathlib import Path
-DATA_PATH = 'data/'
+RAW_DATA_PATH = 'data/'
+PROCESSED_DATA_PATH = 'processed_data/'
 
-entries = os.listdir(DATA_PATH)
-for file_name in entries:
+
+os.mkdir(PROCESSED_DATA_PATH)
+
+raw_data_entries = os.listdir(RAW_DATA_PATH)
+
+for file_name in raw_data_entries:
     print('preprocessing file: ' + file_name)
-    file_name = 'County_zhvi_uc_condo_tier_0.33_0.67_sm_sa_month.csv'
-    dataframe = pandas.read_csv(DATA_PATH + file_name)
+    dataframe = pandas.read_csv(RAW_DATA_PATH + file_name)
     dataframe = dataframe.dropna()
-    filepath = Path('folder/subfolder/out.csv')
+    processed_file_name = file_name.split('.')[0] + '_processed.csv'
+    filepath = Path(PROCESSED_DATA_PATH + processed_file_name)
+    dataframe.to_csv(filepath)
 print('preprocess done')
-filepath.parent.mkdir(parents=True, exist_ok=True)
